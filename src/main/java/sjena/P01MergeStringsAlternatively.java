79cfa679674e4a64f873ee15cb10ae2bf6390286
lizasu <jena.suchismita@gmail.com>
2025-03-07 10:54:35 -0500

    Update P01MergeStringsAlternatively.java

--- src/main/java/sjena/P01MergeStringsAlternatively.java
+++ src/main/java/sjena/P01MergeStringsAlternatively.java
@@ -1,5 +1,20 @@
 public class P01MergeStringsAlternately {
 
   public String mergeAlternately(String word1, String word2) {
+    // loop until bigger word length, append characters from both words until they run out
+        int len1 = word1.length();
+        int len2 = word2.length();
+        StringBuilder sb = new StringBuilder(len1 + len2);
+        int len = Math.max(len1, len2);
+        for(int i = 0; i < len; i++) {
+            if(i < len1) {
+                sb.append(word1.charAt(i));
+            }
+            if(i < len2) {
+                sb.append(word2.charAt(i));
+            }
+        }
+
+        return sb.toString();
   }
 }
